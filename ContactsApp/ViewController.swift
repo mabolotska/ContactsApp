5e5e68bbe6bdbb7f8b97d0923d8b1db400ff3c53
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2024-01-21 21:23:39 +0100

    Initial Commit

---
+++ ContactsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Maryna Bolotska on 21/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
